381efda768a379075e1630790d2033ca11ce7521
rathore23vansh <rathorevansh0023@gmail.com>
2026-05-01 11:33:33 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// import mongoose from "mongoose";
+// mongoose.connect("mongodb://localhost:27017/")
+// .then(()=>console.log("connected to mongodb"))
+// .catch((err)=>console.log("could not connect to mongodb",err));
+
+const fs = require("fs");
+
+fs.readFile("./input.txt","utf-8",(err,data)=>{
+    if(err){
+        console.log("error reading file");
+        return;  
+    }
+    
+    const lines= data.split("\n").length;
+    const words= data.trim().split(/\s+/).length;
+
+    fs.writeFile("output.txt","total lines:"+lines+"\ntotal words:"+words,(err)=>{
+        if(err){
+            console.log("error while writing");
+            return;
+        }
+
+        console.log("Done file Reading and writing");
+
+
+    });
+});
